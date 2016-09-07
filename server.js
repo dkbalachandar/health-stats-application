6f3b36fbfe9e2ede49f3dcbbd47db2ea91dab65e
Balachandar <dkbalachandar@gmail.com>
2016-09-07 11:50:40 -0400

    Changing the port number to 8080

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();                        // create our app w/ express
 var morgan = require('morgan');                // log requests to the console (express4)
 var bodyParser = require('body-parser');         // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
-var port = process.env.PORT || 8081;         // set the port
+var port = process.env.PORT || 8080;         // set the port
 
 
 // configuration ===============================================================
